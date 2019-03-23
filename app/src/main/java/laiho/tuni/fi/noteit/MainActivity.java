6a5cb876f9d5ae690ca321f83f2918e14c6c26d9
Unknown <laiho.lauri@luukku.com>
2019-03-23 14:19:39 +0200

    Add Saving off strings as TextViews
    - Add saving of strings written into EditText as new TextViews for simple notes

--- app/src/main/java/laiho/tuni/fi/noteit/MainActivity.java
+++ app/src/main/java/laiho/tuni/fi/noteit/MainActivity.java
@@ -2,30 +2,34 @@ package laiho.tuni.fi.noteit;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private LinearLayout layoutContent;
+    private int noteAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.saveButton);
+        this.noteAmount = 0;
+        layoutContent = (LinearLayout) this.findViewById(R.id.mainContent);
+        Log.d(TAG, layoutContent.toString());
     }
 
     @Override
@@ -48,5 +52,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean save(View view) {
+        EditText editText = findViewById(R.id.MainEditText);
+        String noteName = "Note" + noteAmount + 1;
+        TextView note = (TextView) new TextView(this);
+        boolean wasSaved = false;
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                (int) LinearLayout.LayoutParams.MATCH_PARENT,
+                (int) LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        note.setText(editText.getText());
+        Log.d(TAG, note.getText().toString());
+        note.setPadding(10,10,10,10);
+        if (!note.getText().toString().equals("") || note.getText().toString() != null) {
+            layoutContent.addView(note, params);
+            editText.setText("");
+            Log.d(TAG, "save: Saved a note!");
+            wasSaved= true;
+        }
+
+        return wasSaved;
     }
 }
