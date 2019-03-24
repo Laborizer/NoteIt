bb21920b1682d79d9ae5fd1b7178dbacb53027af
Unknown <laiho.lauri@luukku.com>
2019-03-24 13:42:39 +0200

    Add basic saving and loading of files
    - Add saveNote() which saves a string into a new file
    - Add loadNote() which opens a file.
    - Add fileFound() which tells if a file of certain name exists

--- app/src/main/java/laiho/tuni/fi/noteit/MainActivity.java
+++ app/src/main/java/laiho/tuni/fi/noteit/MainActivity.java
@@ -11,25 +11,41 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private LinearLayout layoutContent;
+    private ArrayList<String> noteList;
     private int noteAmount;
+    private EditText mainEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate()");
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.saveButton);
-        this.noteAmount = 0;
         layoutContent = (LinearLayout) this.findViewById(R.id.mainContent);
-        Log.d(TAG, layoutContent.toString());
+        this.noteList = new ArrayList<>();
+        this.noteAmount = 1;
+        loadNote("Notes.txt");
+
+        this.mainEditText = findViewById(R.id.MainEditText);
+        this.mainEditText.setText(noteList.get(0));
     }
 
     @Override
@@ -54,27 +70,51 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public boolean save(View view) {
-        EditText editText = findViewById(R.id.MainEditText);
-        String noteName = "Note" + noteAmount + 1;
-        TextView note = (TextView) new TextView(this);
-        boolean wasSaved = false;
+    public void save(View view) {
+        saveNote("Notes.txt");
+    }
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                (int) LinearLayout.LayoutParams.MATCH_PARENT,
-                (int) LinearLayout.LayoutParams.WRAP_CONTENT
-        );
+    public void saveNote(String fileName) {
+        try {
+            OutputStreamWriter out =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(mainEditText.getText().toString());
+            out.close();
+            Toast.makeText(this, "Note saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
 
-        note.setText(editText.getText());
-        Log.d(TAG, note.getText().toString());
-        note.setPadding(10,10,10,10);
-        if (!note.getText().toString().equals("") || note.getText().toString() != null) {
-            layoutContent.addView(note, params);
-            editText.setText("");
-            Log.d(TAG, "save: Saved a note!");
-            wasSaved= true;
+    public boolean fileFound(String fileName) {
+        File file = getBaseContext().getFileStreamPath(fileName);
+        return file.exists();
+    }
+
+    public void loadNote(String fileName) {
+        String noteContent = "";
+        if (fileFound(fileName)) {
+            try {
+                InputStream inputStream = openFileInput(fileName);
+                if (inputStream != null) {
+                    InputStreamReader reader = new InputStreamReader(inputStream);
+                    BufferedReader buffered = new BufferedReader(reader);
+                    String tmp;
+                    StringBuilder builder = new StringBuilder();
+
+                    while((tmp = buffered.readLine()) != null) {
+                        builder.append(tmp + "\n");
+                    }
+                    inputStream.close();
+                    noteContent = builder.toString();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        return wasSaved;
+        this.noteList.add(noteContent);
     }
 }
