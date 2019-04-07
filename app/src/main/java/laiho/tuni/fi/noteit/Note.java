0d3b6e778d946e8db04fcb3574128e2f3c1359fa
Unknown <laiho.lauri@luukku.com>
2019-04-07 13:11:50 +0300

    Add constructor and attributes to Note

--- app/src/main/java/laiho/tuni/fi/noteit/Note.java
+++ app/src/main/java/laiho/tuni/fi/noteit/Note.java
@@ -1,4 +1,51 @@
 package laiho.tuni.fi.noteit;
 
 public class Note {
+    private int id;
+    private String description;
+    private int awardPoints;
+    private boolean isCleared;
+
+    public Note() {
+
+    }
+
+    public Note(int id, String desc, int points, boolean cleared) {
+        setId(id);
+        setDescription(desc);
+        setAwardPoints(points);
+        setCleared(cleared);
+    }
+
+    public int getAwardPoints() {
+        return awardPoints;
+    }
+
+    public void setAwardPoints(int awardPoints) {
+        this.awardPoints = awardPoints;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCleared() {
+        return isCleared;
+    }
+
+    public void setCleared(boolean cleared) {
+        this.isCleared = cleared;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
