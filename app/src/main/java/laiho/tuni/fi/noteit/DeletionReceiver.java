9f7401386a0f2e4bdd6effdd9c0033ed48e2eed7
Unknown <laiho.lauri@luukku.com>
2019-04-22 21:37:17 +0300

    Add DeletionReceiver -class
    - Add DeletionReceiver -class which deletes all note data when invoked

---
+++ app/src/main/java/laiho/tuni/fi/noteit/DeletionReceiver.java
@@ -0,0 +1,15 @@
+package laiho.tuni.fi.noteit;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DeletionReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
