0d82813c700757099aae91eb5f3f60ff1a3043e7
Unknown <laiho.lauri@luukku.com>
2019-04-22 15:37:26 +0300

    Add Javadoc to MainActivity
    - Add javadoc to MainActivity -class
    - Remove unnecessary method call in onCreate()

--- app/src/main/java/laiho/tuni/fi/noteit/MainActivity.java
+++ app/src/main/java/laiho/tuni/fi/noteit/MainActivity.java
@@ -25,17 +25,66 @@ import java.util.List;
 
 import static android.support.v7.widget.RecyclerView.HORIZONTAL;
 
+/**
+ * NoteIt is a simple program that allows users to create little notes for their day and be awarded
+ * for their productivity with points to help motivate them to note things.
+ *
+ * MainActivity is the main class of the application that controls the flow of the application,
+ * the layout and its components.
+ *
+ * @author Lauri Laiho
+ * @version 1.0
+ * @since 2019-03-17
+ */
 public class MainActivity extends AppCompatActivity implements MainRecyclerViewAdapter.ItemClickListener {
 
+    /**
+     * Tag for logging purposes, states the name of the class.
+     */
     private static final String TAG = "MainActivity";
+
+    /**
+     * LinearLayout which holds the main components of the application.
+     */
     private LinearLayout layoutContent;
+
+    /**
+     * EditText component which references the main text field of the app.
+     */
     private EditText mainEditText;
+
+    /**
+     * TextView component which shows and references the amount of points user has collected.
+     */
     private TextView pointView;
+
+    /**
+     * Class which holds and controls the RecyclerView holding and showing the notes.
+     */
     private MainRecyclerViewAdapter adapter;
+
+    /**
+     * Amount of points the user has.
+     */
     private int totalPoints;
+
+    /**
+     * JsonController which controls the flow of JSON data into and from files.
+     */
     private JsonController jsonController;
+
+    /**
+     * List of Notes which are currently in the app.
+     */
     private List<Note> noteList;
 
+    /**
+     * Method that is called when launching the app. It sets the pointers to different layout
+     * components. Also sets the amount of points the user has after it as read it from the files.
+     *
+     * @param savedInstanceState Bundle from which a saved state of the activity can be retrieved
+     *                           from.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,10 +125,14 @@ public class MainActivity extends AppCompatActivity implements MainRecyclerViewA
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
 
-        this.listFromJson();
-
     }
 
+    /**
+     * Method creates and add options -Menu to the toolbar
+     *
+     * @param menu Menu that is created
+     * @return boolean True.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -87,6 +140,12 @@ public class MainActivity extends AppCompatActivity implements MainRecyclerViewA
         return true;
     }
 
+    /**
+     * Method returns true if the pressed menu is settings menu.
+     *
+     * @param item Menu item being pressed.
+     * @return boolean True
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -102,6 +161,13 @@ public class MainActivity extends AppCompatActivity implements MainRecyclerViewA
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Method creates a Note when Save -button is pressed. Note gets the text from the EditText
+     * and is then saved into the Note List. RecyclerView is notified of the data change and the
+     * new List of Notes is written into AllNotes.json file.
+     *
+     * @param view View from which the onClick was invoked.
+     */
     public void save(View view) {
         Note newNote = new Note(mainEditText.getText().toString());
 
@@ -112,29 +178,14 @@ public class MainActivity extends AppCompatActivity implements MainRecyclerViewA
         jsonController.writeJson("AllNotes.json", arr.toString());
     }
 
-    public List<Note> listFromJson() {
-        String JSONString = this.jsonController.readFromFile("AllNotes.json");
-        List<Note> resultList = new ArrayList<>();
-        Log.d(TAG, "listFromJson: " + JSONString);
-        JSONObject currentObject = new JSONObject();
-
-        try {
-            JSONArray arr = new JSONArray(JSONString);
-            for (int i = 0; i < arr.length(); i++) {
-                currentObject = arr.getJSONObject(i);
-                Log.d(TAG, "listFromJson: " + currentObject.getString("NoteContent"));
-                resultList.add(new Note(
-                        currentObject.getString("NoteContent"),
-                        currentObject.getInt("AwardPoints"),
-                        false));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        resultList.toString();
-        return resultList;
-    }
-
+    /**
+     * Method assigns a new Total Point value based on amount of points received from a cleared
+     * Note and the amount user had previously. The amount is also updated on the TextView.
+     *
+     * @param view RecyclerViewItem's radiobutton.
+     * @param position Position of the Item on RecyclerView.
+     * @param points Amount of points the Note had assigned to it.
+     */
     @Override
     public void onItemClick(View view, int position, int points) {
         if (points != 0) {
